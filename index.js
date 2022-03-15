d486a61c33f4f89870119cdbb4520f701a4729c2
Gerson Barrientos <gersonbarrientos62@gmail.com>
2022-03-15 04:19:42 +0000

    added my own pages

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+
+app.get('/', (req, res) => {
+  var title = 'Home Page';
+  res.render('pages/index', {'title':title});
+});
+
+app.get('/immigrant', (req, res) => {
+  var title = 'Immigrant';
+  res.render('pages/immigrant', {'title':title});
+});
+
+app.get('/venezuela', (req, res) => {
+  var title = 'My Story in Venezuela';
+  res.render('pages/venezuela', {'title':title});
+});
+
+app.get('/american-dream', (req, res) => {
+  var title = 'American Dream';
+  res.render('pages/american-dream', {'title':title});
+});
+
+app.get('/links', (req, res) => {
+  var title = 'Links';
+  res.render('pages/links', {'title':title});
+});
+
+app.get('/disclaimer', (req, res) => {
+  var title = 'Disclaimer';
+  res.render('pages/disclaimer', {'title':title});
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
